b740283e50c6b3f4dc3eae136fa0d1113599795f
jayjayjayyoon <windfog2003@gmail.com>
2026-09-22 10:29:57 +0900

    Activate Control Tower V1 navigation and PC/mobile preview on demo homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import '@xyflow/react/dist/style.css';
-import App from './App.jsx';
+import ControlTowerShell from './ControlTowerShell.jsx';
 import './styles.css';
+import './graph.css';
+import './control-tower-shell.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ControlTowerShell />
   </React.StrictMode>,
 );
-import './graph.css';
